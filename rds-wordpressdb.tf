7898ee60fc455edf591c490e34fa1321435bebff
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-11-20 11:37:35 +0100

    added backup_retention_period to rds (no backups for sandbox) and storagetype

--- rds-wordpressdb.tf
+++ rds-wordpressdb.tf
@@ -1,11 +1,15 @@
 resource "aws_db_instance" "wordpressdb" {
   allocated_storage    = 20
+  # max_allocated_storage = 100 # uncomment to enable storage autoscaling. prevents storage full issues
+  backup_retention_period = 0 # backups disabled intentionally for sandbox environment. for production set to at least 7
   engine               = "mysql"
   engine_version       = null # gets the latest version
   identifier           = "jw-capstone-wordpressdb"
   instance_class       = "db.t3.micro"
+  storage_type         = "gp2"
   username             = var.db_user
   password             = var.dbuser_password
+  # delete_protection    = true # uncomment to prevent deletion of RDS instance. Useful for production!
   tags = {
     Name = "jw_capstone_Wordpress_RDS_Database"
   } 
