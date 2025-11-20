4bf09bfae6cb2a82db63d39addd0e883d1595c22
Julian Wuntke <217809968+juwucloud@users.noreply.github.com>
2025-11-20 11:29:38 +0100

    added deregistration_delay for faster scales and matcher for defining healthy instances

--- targetgroup.tf
+++ targetgroup.tf
@@ -5,6 +5,7 @@ resource "aws_lb_target_group" "targetgroup_wordpress" {
   vpc_id   = aws_vpc.capstone_vpc.id
 
   target_type = "instance"
+  deregistration_delay = 30 # waits 30 seconds before deregistering a target
 
   health_check {
     enabled             = true
@@ -15,7 +16,10 @@ resource "aws_lb_target_group" "targetgroup_wordpress" {
     timeout             = 5
     healthy_threshold   = 3
     unhealthy_threshold = 2
+    matcher             = "200-399" # 200 = wordpress site is up, 300s are redirects
   }
+
+  
 
   tags = {
     Name = "targetgroup-wordpress"
